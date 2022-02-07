b613e8ec849157079bf2d40dba133b89bf24e312
Matt Kingshott <51963402+mattkingshott@users.noreply.github.com>
2022-02-07 15:56:01 +0000

    WIP

--- database/migrations/2014_10_12_000000_create_statistics_table.php
+++ database/migrations/2014_10_12_000000_create_statistics_table.php
@@ -4,8 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateStatisticsTable extends Migration
-{
+return new class extends Migration {
     /**
      * Run the migrations.
      *
@@ -29,4 +28,4 @@ class CreateStatisticsTable extends Migration
     {
         Schema::dropIfExists(config('statistics.table', 'statistics'));
     }
-}
+};
